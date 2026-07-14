3d9ce3706f07240c20f9b837ccf909cb9cbd0102
MarshSuffers <murkymarshmallow@gmail.com>
2026-07-14 13:41:20 -0600

    nearly finished
    last thing to add is dynamically updating background and color labels

--- project2/script.js
+++ project2/script.js
@@ -66,9 +66,16 @@ submitBtn.addEventListener("click", btnPressedCol);
 
 function btnPressedCol() {
   let r = slider1.value;
+  let red = "rgb(" + r + ",00,00)";
   let g = slider2.value;
+  let green = "rgb(00," + g + ",00)";
   let b = slider3.value;
+  let blue = "rgb(00,00," + b + ")";
   let coval = "rgb(" + r + ", " + b + ", " + g + ")";
+
+  document.getElementById("rLabel").style.color = red;
+  document.getElementById("gLabel").style.color = green;
+  document.getElementById("bLabel").style.color = blue;
   document.getElementById("bigContainer").style.backgroundColor = coval;
 }
 
@@ -179,24 +186,78 @@ function something() {
 evilBtn.addEventListener("click", changeEvil);
 
 function changeEvil() {
+  const evilLaugh = new Audio("sounds/evilLaugh.mp3");
+  const evilMusic = new Audio("sounds/scaryMusic.mp3");
+
   document.getElementById("bigContainer").style.display = "none";
+  document.getElementById("exImage").src = "images/scaryFaceLaugh.png";
+  document.getElementById("exDiv").style.display = "block";
+
+  setTimeout(monologue, 3000);
+
+  function monologue() {
+    document.getElementById("exImage").src = "images/scaryFaceSmile.png";
+  }
+
+  music.pause();
+  evilLaugh.play();
+  evilMusic.play();
 }
 
 //Fun Button
 funBtn.addEventListener("click", changeFun);
 
 function changeFun() {
+  const funMusic = new Audio("sounds/8bitFunMusic.mp3");
+
+  music.pause();
+  funMusic.play();
+
+  funBtn.removeEventListener("click", changeFun);
+
   document.getElementById("bigContainer").style.backgroundImage =
     "url('images/rainbow.gif')";
   document.getElementById("remark").innerText = "Fun! ... for ten seconds.";
+
   function unfun() {
+    funMusic.pause();
+    music.play();
+
     document.getElementById("bigContainer").style.backgroundImage = "none";
     document.getElementById("remark").innerText = "Aww, funs over...";
+
+    funBtn.addEventListener("click", changeFun);
   }
   setTimeout(unfun, 10000);
 }
 
-//Quote Button - add
+//Quote Button
+const quoteArray = [
+  "JARONA - Flowery, Deltarune chapter 5",
+  "Hi! I like shorts! They're comfy and easy to wear! - Youngster Ben, Pokemon Red&Blue",
+  "I used to be an adventurer like you. Then I took an arrow in the knee. - Guard, Skyrim",
+  "God has cursed me for my hubris, and my work is never finished. - Brian David Gilbert, real person",
+  "MUSIC MAAAAAAAANNNNNNNN!!!!!!!!!! - Mathew Patrick(Matpat), Game Theory",
+];
+console.log(quoteArray);
+
+quoteBtn.addEventListener("click", quote);
+
+function quote() {
+  let i = quoteArray.length;
+  while (i != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * i);
+    i--;
+
+    // And swap it with the current element.
+    [quoteArray[i], quoteArray[randomIndex]] = [
+      quoteArray[randomIndex],
+      quoteArray[i],
+    ];
+  }
+  document.getElementById("remark").innerText = quoteArray[0];
+}
 
 //Hide Button
 hideBtn.addEventListener("click", keepHiding);
@@ -208,8 +269,21 @@ function keepHiding() {
 //Explode
 explodeBtn.addEventListener("click", explode);
 
+const explodeSound = new Audio("sounds/deltarune-explosion.mp3");
+const interval = 1525;
+
 function explode() {
+  music.pause();
+
   document.getElementById("exDiv").style.display = "block";
   console.log(exDiv.style.display);
   document.getElementById("bigContainer").style.display = "none";
+
+  setInterval(() => {
+    explodeSound.currentTime = 0; // Reset audio to the beginning
+    explodeSound.play();
+  }, interval);
 }
+
+//Set music as object
+const music = document.getElementById("music");
