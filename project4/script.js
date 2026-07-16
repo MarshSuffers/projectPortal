4445bac7b6be39eda12c80258b9d32f9eeb1ac57
MarshSuffers <murkymarshmallow@gmail.com>
2026-07-16 13:44:22 -0600

    finished settings
    finished settings, added some tile functionality

--- project4/script.js
+++ project4/script.js
@@ -66,8 +66,53 @@ function playPeppy() {
 }
 
 //Color Change
+colorBtnGrey.addEventListener("click", turnGrey);
+colorBtnGreen.addEventListener("click", turnGreen);
+colorBtnBlue.addEventListener("click", turnBlue);
+
+const sections = document.getElementsByTagName("section");
+
+function turnGrey() {
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.backgroundColor = "lightgray";
+    sections[i].style.borderColor = "gray";
+  }
+}
+
+function turnGreen() {
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.backgroundColor = "lightgreen";
+    sections[i].style.borderColor = "green";
+  }
+}
+
+function turnBlue() {
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.backgroundColor = "lightskyblue";
+    sections[i].style.borderColor = "blue";
+  }
+}
 
 //Create Tiles
+
+//tile array
+const tileArray = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+  41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+  60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+  79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
+  98, 99, 100, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+  115, 116, 117, 118, 119, 120,
+];
+let mineGrid = document.getElementById("mineGrid");
+
+for (let i = 0; i < tileArray.length; i++) {
+  mineGrid.innerHTML += `<div class="cubes">
+        <button class="cubeBtn" id="cubeBtn1"></button>
+      </div>`;
+  console.log(mineGrid.innerHTML);
+}
 
 //Game Over
 
