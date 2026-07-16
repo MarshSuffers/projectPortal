1414b4588854c50d39e5935d0ef0b80ec9a32a13
MarshSuffers <murkymarshmallow@gmail.com>
2026-07-16 12:26:19 -0600

    wtf
    weird ass bug, hoping commit will fix

--- project4/script.js
+++ project4/script.js
@@ -7,7 +7,7 @@ setInterval(setTime, 1000);
 function setTime() {
   ++totalSeconds;
   seconds.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.inner = pad(parseInt(totalSeconds / 60));
+  minutes.inner = pad(parseInt(totalSeconds / 60));
 }
 
 function pad(val) {
@@ -23,10 +23,49 @@ function pad(val) {
 
 //Music
 songBtnCalm.addEventListener("click", playCalm);
+songBtnCheery.addEventListener("click", playCheery);
+songBtnPeppy.addEventListener("click", playPeppy);
+
+const calmMusic = new Audio("sounds/calmMusic.mp3");
+const cheeryMusic = new Audio("sounds/cheeryMusic.mp3");
+const peppyMusic = new Audio("sounds/peppyMusic.mp3");
+
+let music = false;
 
 function playCalm() {
-  const calmMusic = new Audio("sounds/calmMusic.mp3");
-  calmMusic.play();
+  if (music == true) {
+    cheeryMusic.pause;
+    peppyMusic.pause;
+    calmMusic.pause();
+    music = false;
+  } else {
+    calmMusic.play();
+    music = true;
+    console.log(music);
+  }
+}
+function playCheery() {
+  if (music == true) {
+    cheeryMusic.pause;
+    peppyMusic.pause;
+    calmMusic.pause();
+    console.log(music);
+    music = false;
+  } else {
+    cheeryMusic.play();
+    music = true;
+  }
+}
+function playPeppy() {
+  if (music == true) {
+    cheeryMusic.pause;
+    peppyMusic.pause;
+    calmMusic.pause();
+    music = false;
+  } else {
+    peppyMusic.play();
+    music = true;
+  }
 }
 
 //Color Change
