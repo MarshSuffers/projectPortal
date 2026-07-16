aa8cbcb1f2786fdb77a7cae3ada43ea4be48ac07
MarshSuffers <murkymarshmallow@gmail.com>
2026-07-16 09:33:09 -0600

    finished assignment
    finished dynamic cards assignment

--- script.js
+++ script.js
@@ -11,18 +11,47 @@ let year = document.querySelector("#year");
 year.textContent = currentYear;
 
 //Dynamic Cards
+//declare map
+let cards = new Map();
 
+cards.set("HTML Project", "project1/index.html");
+cards.set("Interactive Website", "project2/index.html");
+cards.set("Empty Project", "project3/index.html");
+cards.set("Minesweeper", "project4/index.html");
+
+//declare variables
 let cardsList = document.querySelector(".cards");
-let titles = [];
-let links = [];
-let map = new Map();
+let navLinks = document.getElementById("navList");
+let titles = cards.keys();
+let links = cards.values();
+let i = 0;
 
-for (let i = 0; i < titles.length; i++) {
-  // mp = Map
-  // keys = key array
-  // values = value array
-  map.set(titles[i], links[i]);
-}
-for (let key of map.titles()) {
-  console.log(key + " => " + map.get(key));
+//declare arrays
+let titlesArray = Array.from(titles);
+let linksArray = Array.from(links);
+
+//iterate through each set
+cards.forEach((values, keys) => {
+  console.log(values, keys);
+
+  cardsList.innerHTML += `<div class="card">
+        <div class="cardInner">
+          <div class="cardFront">
+            <h2>${titlesArray[i]}</h2>
+          </div>
+          <div class="cardBack">
+            <a href="${linksArray[i]}">Project link</a>
+          </div>
+        </div>
+      </div>`;
+
+  i++;
+  console.log(i);
+});
+
+//Dynamic Links
+
+for (let l = 0; l < linksArray.length; l++) {
+  console.log(linksArray[l]);
+  navLinks.innerHTML += `<li><a href="${linksArray[l]}">${titlesArray[l]}</a></li>`;
 }
